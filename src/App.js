f805f652a9284cdeccf4bcd3d840ffeace54f222
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-08-06 15:35:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import './App.css';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Header,
+  WhatGPT3,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Article from './article/Article';
+import Brand from './brand/Brand';
+import CTA from './cta/CTA';
+import Feature from './feature/Feature';
+import Navbar from './navbar/Navbar';
+
+export { Article, Brand, CTA, Feature, Navbar };
